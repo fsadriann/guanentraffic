571a6844b41bdf1c8dbca94d014bca6885d8ff43
unknown <adrianforero@colegioguanenta.edu.co>
2023-09-29 22:20:41 -0500

    ya mejoramos

---
+++ gui_ex.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+import cv2
+import imutils
+
+def visualizar():
+    global cap
+    if cap is not None:
+        ret, frame = cap.read()
+        if ret == True:
+            frame = imutils.resize(frame, width=640)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            im = image.fromarray(frame)
+            img = ImageTk.PhotoImage(image=im)
+
+            lblVideo.configure(image=img)
+            lblVideo.image = img
+            lblVideo.after(10, visualizar)
+        else:
+            lblVideo.image = ""
+            cap.release
+
+
+def iniciar():
+    global cap
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    visualizar()
+
+cap = None
+root = Tk()
+
+btnIniciar = Button(root,text="Iniciar", width=45,command=iniciar)
+btnIniciar.grid(column=0,row=0, padx=5,pady=5)
+
+btnFinalizar = Button(root,text="Finalizar", width=45)
+btnFinalizar.grid(column=1,row=0, padx=5,pady=5)
+
+lblVideo = Label(root)
+lblVideo.grid(column=0,row=1, columnspan=2)
+
+root.mainloop()
